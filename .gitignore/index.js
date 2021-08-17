271f000e478b31379443f8107ea4ef089b380e2a
BestPaladin <45981792+BestPaladin@users.noreply.github.com>
2021-08-17 16:38:32 +1100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,30 +9,32 @@ client.once('ready', () => {
 client.login(process.env.BOT_TOKEN);
 
 client.on("message", message => {
-  if (message.content === "!ping") {
-    message.channel.send("Pong.");
-  } else if (message.content.endsWith("quoi")) {
-     message.channel.send("feur");
-  } else if (message.content === "!regles") {
-    message.delete();
-    
-    const embed = new Discord.RichEmbed()
-      .setTitle("Réglement officiel du serveur discord")
-      .setColor('#006eff')
-      .setDescription("Lors de la navigation dans l'enceinte du serveur, le joueur s'engage à avoir lu ce réglement.")
-      .setFooter("Merci de valider ci-dessous le réglement.")
-      .addField(':no_entry: De spams', '\u200b')
-      .addField(':no_entry: De donner des informations personnelles', '\u200b')
-      .addField(':no_entry: De faire du forcing IRL', '\u200b')
-      .addField(':no_entry: D\'insulter', '\u200b')
-      .addField(':no_entry: De pub pour d\'autres serveurs', '\u200b')
-      .addField(':no_entry: De manquer de respect', '\u200b')
-      .addField(':no_entry: Racisme même en blague', '\u200b')
-      .addField(':no_entry: D\'avoir un pseudo insultant', '\u200b')
-      .addField(':no_entry: D\'avoir un pseudo similaire à un staff', '\u200b')
-      .addField(':no_entry: D\'avoir un pseudo similaire à une personne connue IRL', '\u200b')
-      .setThumbnail("https://cdn.discordapp.com/icons/876435753121488906/c3795293709c2238efe5fb7d14c3544d.png");
-    message.channel.send({embed})
+  switch(message.content) {
+    case 'sd!ping':
+      message.channel.send("Pong.");
+      break;
+    case 'sd!regles':
+      message.delete();
+      const embed = new Discord.RichEmbed()
+        .setTitle("Réglement officiel du serveur discord")
+        .setColor('#006eff')
+        .setDescription("Lors de la navigation dans l'enceinte du serveur, le joueur s'engage à avoir lu ce réglement.")
+        .setFooter("Merci de valider ci-dessous le réglement.")
+        .addField(':no_entry: De spams', '\u200b')
+        .addField(':no_entry: De donner des informations personnelles', '\u200b')
+        .addField(':no_entry: De faire du forcing IRL', '\u200b')
+        .addField(':no_entry: D\'insulter', '\u200b')
+        .addField(':no_entry: De pub pour d\'autres serveurs', '\u200b')
+        .addField(':no_entry: De manquer de respect', '\u200b')
+        .addField(':no_entry: Racisme même en blague', '\u200b')
+        .addField(':no_entry: D\'avoir un pseudo insultant', '\u200b')
+        .addField(':no_entry: D\'avoir un pseudo similaire à un staff', '\u200b')
+        .addField(':no_entry: D\'avoir un pseudo similaire à une personne connue IRL', '\u200b')
+        .setThumbnail("https://cdn.discordapp.com/icons/876435753121488906/c3795293709c2238efe5fb7d14c3544d.png");
+      message.channel.send({embed})
+      break;
+    case default:
+      break;
   }
 });
 
